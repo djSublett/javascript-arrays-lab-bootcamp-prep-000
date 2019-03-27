df9f339492b33cc6cde104204275032b2b08408f
Dale Sublett <djsublett04@gmail.com>
2019-03-27 00:54:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,3 +5,6 @@ var kittens = ['Milo', 'Otis', 'Garfield'] //define your array here
 function destructivelyAppendKitten(name) {
   kittens.push(name)
 }
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name)
+}
